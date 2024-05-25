8e7f2bce9802cddd69246f649e2ef09a71809dcc
M4t7e021 <00030123@uca.edu.sv>
2024-05-25 13:11:35 -0600

    Subiendo al repo

---
+++ SegundoParcial/src/Alipay.java
@@ -0,0 +1,14 @@
+public class Alipay {
+    private String nombreUsuario;
+    private String nombreEmpresa;
+
+    public Alipay() {
+    }
+
+    public Alipay( String nombreUsuario , String nombreEmpresa) {
+        this.nombreUsuario = nombreUsuario;
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+
+}
